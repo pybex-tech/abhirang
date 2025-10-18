a512c373dfa0f37a704e38f0a433257ecbc19940
an-anurag <an.anurag@msn.com>
2025-10-18 21:59:22 +0530

    fixed-urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/', profile_view, name='profile'),
-        path('logout/', logout_view, name='logout'),
-    path('profile/', profile_view, name='profile'),
 ]
