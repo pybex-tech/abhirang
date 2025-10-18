1f4ce8cbdf370d46aff8c13232d0b62e8d53c4c3
an-anurag <an.anurag@msn.com>
2025-10-18 21:32:53 +0530

    updated urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/', profile_view, name='profile'),
+        path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
 ]
